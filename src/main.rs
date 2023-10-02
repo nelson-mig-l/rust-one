bd599ce695f7e1b43306ae96e7a0d9097a1311ae
Nelson Miguel <nelson.mig.l@gmail.com>
2023-10-02 20:32:02 +0000

    crates

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,30 @@
+extern crate atty;
+extern crate wkt;
+
 use std::io::{BufRead, stdin};
+use std::str::FromStr;
+use clap::Parser;
+
+#[derive(Parser)]
+struct Args {
+    #[arg(short, long, default_value = "*")]
+    pattern: String,
+}
 
 fn main() {
     println!("Hello, world!");
-    stdin()
-        .lock()
-        .lines()
-        .for_each(|line| println!("{}", line.unwrap()));
+    let args = Args::parse();
+    println!("{}", &args.pattern);
+    if atty::isnt(atty::Stream::Stdin) {
+        stdin()
+            .lock()
+            .lines()
+            .for_each(|line| println!("{}", line.unwrap()));
+    }
+    let s = "POLYGON ((35 10, 45 45, 15 40, 10 20, 35 10), (20 30, 35 35, 30 20, 20 30))";
+    let g = wkt::Wkt::<f64>::from_str(s).unwrap();
+
+    println!("{}", g);
+
+    println!("Goodbye, world!"); 
 }
